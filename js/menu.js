cfb0897ccdf3341920c95e0177a24d9c2b0d4d93
ym315 <ym021315@gamil.com>
2024-04-15 13:45:10 +0900

    up

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    //start    
+
+
+
+        $('header').load('menu.html header>div');
+        $('.main_left').load('menu.html .main_menu, menu.html .main_left_dummy');
+        $('.main_left_dummy').load('menu.html .main_left_dummy>div');
+        $('footer').load('menu.html footer>div');
+        // $('aside').load('menu.html aside');   
+
+
+        $('.btn_close').on('click',function(e){
+            e.preventDefault();
+            $(this).parents().find('.popupdiv').hide();            
+
+        });
+    
+    
+    
+});
+
